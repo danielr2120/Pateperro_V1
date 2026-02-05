960456a564585be14f19cd2eed54bd01803acbf2
danielr2120 <danielrevelo2120@gmail.com>
2026-02-05 10:18:48 -0500

    Corrijo Nombres de las imagenes

--- src/pages/Expo_cafe.jsx
+++ src/pages/Expo_cafe.jsx
@@ -2,12 +2,12 @@ import img1 from "../assets/images/img1.png"
 import img2 from "../assets/images/img2.png"
 import img3 from "../assets/images/img3.png"
 import img4 from "../assets/images/img4.png"
-import img5 from "../assets/images/img5.png"
+import img5 from "../assets/images/img5.jpg"
 import img6 from "../assets/images/img6.png"
-import img7 from "../assets/images/img7.png"
-import img8 from "../assets/images/img8.png"
-import img9 from "../assets/images/img9.png"
-import img10 from "../assets/images/img10.png"
+import img7 from "../assets/images/img7.jpg"
+import img8 from "../assets/images/img8.jpg"
+import img9 from "../assets/images/img9.jpg"
+import img10 from "../assets/images/img10.jpg"
 
 
 function ExpoCafe(){
