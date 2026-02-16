a6804340dd5cea5c9b946d337804d4a66dbef584
danielr2120 <danielrevelo2120@gmail.com>
2026-02-16 11:43:52 -0500

    Se agrega algo de css para hacer el sitio responsive para moviles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,44 @@
-import logo from '../assets/Logo2.png'
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import logo from '../assets/Logo2.png';
+import { Link } from "react-router-dom";
+
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className= "header">
-     
-      <nav className='nav-left'>
+    <header className="header">
+
+      {/* Navegación desktop */}
+      <nav className={`nav-left ${menuOpen ? "active" : ""}`}>
         <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/Galeria">Galeria</Link></li>
-          <li><Link to="/ExpoCafe">Expo Cafe</Link></li>
-          <li><Link to="/Contacto">Contacto</Link></li>
+          <li><Link to="/" onClick={() => setMenuOpen(false)}>Inicio</Link></li>
+          <li><Link to="/Galeria" onClick={() => setMenuOpen(false)}>Galeria</Link></li>
+          <li><Link to="/ExpoCafe" onClick={() => setMenuOpen(false)}>Expo Cafe</Link></li>
+          <li><Link to="/Contacto" onClick={() => setMenuOpen(false)}>Contacto</Link></li>
         </ul>
       </nav>
-      <li>
+
+      {/* Logo centrado */}
+      <div className="logo-container">
         <Link to="/">
-        <img src={logo} alt="Logo PatePerro" width="186x" height="43px" className='logo-img'/>
+          <img 
+            src={logo} 
+            alt="Logo PatePerro" 
+            className="logo-img"
+          />
         </Link>
-      </li>
-      
+      </div>
+
+      {/* Botón hamburguesa */}
+      <div 
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
     </header>
-  )
+  );
 }
-export default Header
+
+export default Header;
