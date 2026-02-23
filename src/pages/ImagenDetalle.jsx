1b8d7f71ce12285b0b33a457b9785523144a596d
danielr2120 <danielrevelo2120@gmail.com>
2026-02-23 11:32:21 -0500

    Se agrega información a la imagen

--- src/pages/ImagenDetalle.jsx
+++ src/pages/ImagenDetalle.jsx
@@ -1,11 +1,12 @@
 import { useParams, useLocation } from "react-router-dom";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import images from "../data/imagenes.json";
 
 function ImagenDetalle() {
   const { id } = useParams();
   const location = useLocation();
   const imageRef = useRef(null);
+  const [showTooltip, setShowTooltip] = useState(false);
 
   let seccion = "";
   if (location.pathname.startsWith("/expocafe")) {
@@ -15,9 +16,7 @@ function ImagenDetalle() {
   }
 
   const image = images.find(
-    (img) =>
-      String(img.id) === String(id) &&
-      img.seccion === seccion
+    (img) => String(img.id) === String(id) && img.seccion === seccion
   );
 
   useEffect(() => {
@@ -32,20 +31,38 @@ function ImagenDetalle() {
   if (!image) {
     return <p>Imagen no encontrada</p>;
   }
+
   return (
     <main className="imagen-detalle">
       <div className="detalle-contenido">
-        <div className="detalle-imagen">
+        <div className="detalle-imagen" style={{ position: "relative" }}>
           <img
             src={image.url}
             alt={image.nombre || image.autor}
+            ref={imageRef}
           />
+          <div className="info-icon"
+            onMouseEnter={() => setShowTooltip(true)}
+            onMouseLeave={() => setShowTooltip(false)}
+          >
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=info" />
+          <span class="material-symbols-outlined">
+          info
+          </span>
+          {showTooltip && (
+            <div className="tooltip">
+              <strong>{image.camara}</strong>
+              <br />
+              <strong>{image.especificaciones}</strong>
+            </div>
+          )}
+          </div>
           <span className="pie-imagen">
-          <strong>{image.nombre}{" - "}{image.autor}</strong>
-          <br />
-          <strong>{image.lugar}</strong>
-          <br />
-          <strong>{image.fecha}</strong>
+            <strong>{image.nombre} - {image.autor}</strong>
+            <br />
+            <strong>{image.lugar}</strong>
+            <br />
+            <strong>{image.fecha}</strong>
           </span>
         </div>
       </div>
